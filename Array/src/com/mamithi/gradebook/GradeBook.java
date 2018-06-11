9ef5ae8c76c89297fc58497826e3648268afd9a7
Mamithi <19lawrence93@gmail.com>
2018-06-11 16:05:16 +0300

    Multidimensional arrays

--- Array/src/com/mamithi/gradebook/GradeBook.java
+++ Array/src/com/mamithi/gradebook/GradeBook.java
@@ -2,9 +2,9 @@ package com.mamithi.gradebook;
 
 public class GradeBook {
     private String courseName;
-    private int[] grades;
+    private int[][] grades;
 
-    public GradeBook(String name, int[] gradesArray) {
+    public GradeBook(String name, int[][] gradesArray) {
         courseName = name;
         grades = gradesArray;
     }
@@ -24,76 +24,93 @@ public class GradeBook {
     public void proceessGrades() {
         outputGrades();
 
-        System.out.printf("\n Class average is %.2f\n", getAverage());
-
         System.out.printf("Lowest grade is %d\nHighest grade is %d\n\n", getMinimum(), getMaximum());
 
         outputBarChart();
     }
 
-    public int getMinimum(){
-        int lowestGrade = grades[0];
+    public int getMinimum() {
+        int lowestGrade = grades[0][0];
 
-        for( int grade: grades){
-            if(grade < lowestGrade){
-                lowestGrade = grade;
+        for (int[] studentGrades : grades) {
+            for (int grade : studentGrades) {
+                if (grade < lowestGrade) {
+                    lowestGrade = grade;
+                }
             }
+
         }
         return lowestGrade;
     }
 
     public int getMaximum() {
-        int highGrade = grades[0];
-
-        for (int grade : grades) {
-            if (grade > highGrade) {
-                highGrade = grade;
+        int highGrade = grades[0][0];
+        for (int[] studentGrades : grades) {
+            for (int grade : studentGrades) {
+                if (grade > highGrade) {
+                    highGrade = grade;
+                }
             }
         }
         return highGrade;
     }
 
-    public double getAverage() {
+    public double getAverage(int[] setOfGrades) {
         int total = 0;
 
-        for (int grade : grades) {
+        for (int grade : setOfGrades) {
             total += grade;
         }
 
         return (double) total / grades.length;
     }
 
-    public void outputBarChart(){
+    public void outputBarChart() {
         System.out.println("Grades Distribution:");
 
         int[] frequency = new int[11];
 
-        for(int grade : grades){
-            ++frequency[grade/10];
+        for (int[] studentGrades : grades) {
+            for (int grade : studentGrades) {
+                ++frequency[grade / 10];
+            }
         }
 
-        for(int count = 0; count < frequency.length; count++){
-            if(count == 10){
+        for (int count = 0; count < frequency.length; count++) {
+            if (count == 10) {
                 System.out.printf("%5d: ", 100);
-            }else{
-                System.out.printf("%02d-%02d: ", count * 10, count*10 + 9);
+            } else {
+                System.out.printf("%02d-%02d: ", count * 10, count * 10 + 9);
             }
 
-            for(int stars = 0; stars < frequency[count]; stars++){
+            for (int stars = 0; stars < frequency[count]; stars++) {
                 System.out.print("*");
             }
 
-           System.out.println();
+            System.out.println();
         }
 
 
     }
 
-    public void outputGrades(){
+    public void outputGrades() {
         System.out.println("The grades are:\n");
+        System.out.print("               ");
 
-        for(int student = 0; student < grades.length; student++){
-            System.out.printf("Student %2d: %3d\n", student + 1, grades[student]);
+        for (int test = 0; test < grades[0].length; test++) {
+            System.out.printf("Test %d ", test + 1);
         }
+
+        System.out.println("Average");
+
+        for (int student = 0; student < grades.length; student++) {
+            System.out.printf("Student %2d", student + 1);
+            for (int test : grades[student]) {
+                System.out.printf("%8d", test);
+            }
+            double average = getAverage(grades[student]);
+            System.out.printf("%9.2f\n", average);
+        }
+
     }
 }
