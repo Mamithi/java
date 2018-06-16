27b3da926cbd596a4fa993111ee045bf43266a22
Mamithi <19lawrence93@gmail.com>
2018-06-16 19:44:36 +0300

    Implementing inheritance

--- Inheritance/src/com/mamithi/BasePlusCommissionEmployee.java
+++ Inheritance/src/com/mamithi/BasePlusCommissionEmployee.java
@@ -1,71 +1,15 @@
 package com.mamithi;
 
-public class BasePlusCommissionEmployee {
-    private String firstname, lastname, socialSecurityNumber;
-    private double grossSales, commissionRate, baseSalary;
+public class BasePlusCommissionEmployee extends CommissionEmployee {
+    private double baseSalary;
 
     public BasePlusCommissionEmployee(String first, String last,
                                       String ssn, double sales,
                                       double rate, double salary) {
-        firstname = first;
-        lastname = last;
-        socialSecurityNumber = ssn;
-        setGrossSales(sales);
-        setCommissionRate(rate);
+        super(first, last, ssn, sales, rate);
         setBaseSalary(salary);
     }
 
-    public void setFirstname(String first) {
-        firstname = first;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setLastname(String last) {
-        lastname = last;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setSocialSecurityNumber(String ssn) {
-        socialSecurityNumber = ssn;
-    }
-
-    public String getSocialSecurityNumber() {
-        return socialSecurityNumber;
-    }
-
-    public void setGrossSales(double sales) {
-        if (sales >= 0.0) {
-            grossSales = sales;
-        } else {
-            throw new IllegalArgumentException(
-                    "Gross sales must be >= 0.0"
-            );
-        }
-    }
-
-    public double getGrossSales() {
-        return grossSales;
-    }
-
-    public void setCommissionRate(double rate) {
-        if (rate > 0.0 && rate < 1.0) {
-            commissionRate = rate;
-        } else {
-            throw new IllegalArgumentException(
-                    "Commission rate must be > 0 and < 1.0"
-            );
-        }
-    }
-
-    public double getCommissionRate() {
-        return commissionRate;
-    }
 
     public void setBaseSalary(double salary) {
         if (salary >= 0.0) {
@@ -81,18 +25,15 @@ public class BasePlusCommissionEmployee {
         return baseSalary;
     }
 
+    @Override
     public double earnings() {
-        return baseSalary + (commissionRate * grossSales);
+        return baseSalary + super.earnings();
     }
 
     @Override
     public String toString() {
-        return String.format(
-                "%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n %s: %.2f ",
-                "base-salaried commission employee", firstname, lastname,
-                "social security number", socialSecurityNumber,
-                "gross sales", grossSales, "commission rate", commissionRate,
-                "base salary", baseSalary
+        return String.format("%s %s\n%s: %.2f", "base-salaried",
+                super.toString(), "base salary", getBaseSalary()
         );
     }
 }
--- Inheritance/src/com/mamithi/CommissionEmployee.java
+++ Inheritance/src/com/mamithi/CommissionEmployee.java
@@ -67,16 +67,16 @@ public class CommissionEmployee extends Object {
     }
 
     public double earnings() {
-        return commissionRate * grossSales;
+        return getCommissionRate() * getGrossSales();
     }
 
     @Override
     public String toString() {
         return String.format("%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f",
-                "comission employee", firstname, lastname,
-                "social security number", socialSecurityNumber,
-                "gross sales", grossSales,
-                "commission rate", commissionRate
+                "comission employee", getFirstname(), getLastname(),
+                "social security number", getSocialSecurityNumber(),
+                "gross sales", getGrossSales(),
+                "commission rate", getCommissionRate()
         );
     }
 }
