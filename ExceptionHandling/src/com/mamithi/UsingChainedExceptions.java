ed858c9693ddb489d364a0f3ccfc7942faf41f85
Mamithi <19lawrence93@gmail.com>
2018-06-20 00:28:22 +0300

    Using chained exceptions

---
+++ ExceptionHandling/src/com/mamithi/UsingChainedExceptions.java
@@ -0,0 +1,33 @@
+package com.mamithi;
+
+public class UsingChainedExceptions {
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void method1() throws Exception {
+        try {
+            method2();
+        } catch (Exception exception) {
+            throw new Exception("exception thrown in method1", exception);
+        }
+    }
+
+    public static void method2() throws Exception {
+        try {
+            method3();
+        } catch (Exception exception) {
+            throw new Exception("Exception thrown in method2", exception);
+        }
+    }
+
+    public static void method3() throws Exception {
+        throw new Exception("Exception thrown in method3");
+    }
+
+
+}
