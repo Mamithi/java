14cd83de70f77559c04bde83900a6667e43801e5
Mamithi <19lawrence93@gmail.com>
2018-07-10 10:53:58 +0300

    Learning about JCheckbox

---
+++ GUIComponentPart1/src/com/mamithi/CheckBoxTest.java
@@ -0,0 +1,13 @@
+package com.mamithi;
+
+import javax.swing.*;
+
+public class CheckBoxTest {
+    public static void main(String[] args){
+        CheckBoxFrame checkBoxFrame = new CheckBoxFrame();
+        checkBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        checkBoxFrame.setSize(257, 100);
+        checkBoxFrame.setVisible(true);
+
+    }
+}
