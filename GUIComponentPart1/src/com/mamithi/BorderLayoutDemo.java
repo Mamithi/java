6f92392310e346e612ff33b054b9519ea7adb687
Mamithi <19lawrence93@gmail.com>
2018-07-11 13:57:58 +0300

    Learning border layout

---
+++ GUIComponentPart1/src/com/mamithi/BorderLayoutDemo.java
@@ -0,0 +1,13 @@
+package com.mamithi;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BorderLayoutDemo {
+    public static void main(String[] args){
+        BorderLayoutFrame borderLayoutFrame = new BorderLayoutFrame();
+        borderLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        borderLayoutFrame.setSize(300, 200);
+        borderLayoutFrame.setVisible(true);
+    }
+}
