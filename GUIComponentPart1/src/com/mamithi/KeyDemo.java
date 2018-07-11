1e70ef81a5ac6e3776e541fccd1d7b34be6e0578
Mamithi <19lawrence93@gmail.com>
2018-07-11 13:08:28 +0300

    Learning key events

---
+++ GUIComponentPart1/src/com/mamithi/KeyDemo.java
@@ -0,0 +1,12 @@
+package com.mamithi;
+
+import javax.swing.*;
+
+public class KeyDemo {
+    public static void main(String[] args){
+        KeyDemoFrame keyDemoFrame = new KeyDemoFrame();
+        keyDemoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        keyDemoFrame.setSize(350, 100);
+        keyDemoFrame.setVisible(true);
+    }
+}
