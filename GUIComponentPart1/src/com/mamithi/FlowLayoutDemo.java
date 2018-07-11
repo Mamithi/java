210c03d6aef93e19709b867dde6c666666210f68
Mamithi <19lawrence93@gmail.com>
2018-07-11 13:32:04 +0300

    Learning flow layout

---
+++ GUIComponentPart1/src/com/mamithi/FlowLayoutDemo.java
@@ -0,0 +1,12 @@
+package com.mamithi;
+
+import javax.swing.*;
+
+public class FlowLayoutDemo {
+    public static void main(String[] args){
+        FlowLayoutFrame flowLayoutFrame = new FlowLayoutFrame();
+        flowLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        flowLayoutFrame.setSize(200, 275);
+        flowLayoutFrame.setVisible(true);
+    }
+}
---
+++ GUIComponentPart1/src/com/mamithi/FlowLayoutFrame.java
@@ -0,0 +1,57 @@
+package com.mamithi;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FlowLayoutFrame extends JFrame {
+    private JButton leftJButton, centerJButton, rightJButton;
+    private FlowLayout layout;
+    private Container container;
+
+    public FlowLayoutFrame(){
+        super("FlowLayout Demo");
+
+        layout = new FlowLayout();
+        container = getContentPane();
+        setLayout(layout);
+
+        leftJButton = new JButton("Left");
+        add(leftJButton);
+        leftJButton.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent event) {
+                        layout.setAlignment(FlowLayout.LEFT);
+
+                        layout.layoutContainer(container);
+                    }
+                }
+        );
+
+        centerJButton = new JButton("Center");
+        add(centerJButton);
+
+        centerJButton.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent event) {
+                        layout.setAlignment(FlowLayout.CENTER);
+
+                        layout.layoutContainer(container);
+                    }
+                }
+        );
+
+        rightJButton = new JButton("Right");
+        add(rightJButton);
+        rightJButton.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent event) {
+                        layout.setAlignment(FlowLayout.RIGHT);
+
+                        layout.layoutContainer(container);
+                    }
+                }
+        );
+    }
+}
