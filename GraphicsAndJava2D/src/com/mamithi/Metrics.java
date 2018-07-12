34aa508a2069fcf30d726b844f3bc02ad08f9c0e
Mamithi <19lawrence93@gmail.com>
2018-07-12 11:04:14 +0300

    Learning about font metrics

---
+++ GraphicsAndJava2D/src/com/mamithi/Metrics.java
@@ -0,0 +1,16 @@
+package com.mamithi;
+
+import javax.swing.*;
+
+public class Metrics {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Demonstrating FontMetrics");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MetricsJPanel metricsJPanel = new MetricsJPanel();
+        frame.add(metricsJPanel);
+        frame.setSize(510, 240);
+        frame.setVisible(true);
+    }
+
+}
---
+++ GraphicsAndJava2D/src/com/mamithi/MetricsJPanel.java
@@ -0,0 +1,27 @@
+package com.mamithi;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MetricsJPanel extends JPanel {
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.setFont(new Font("SansSerif", Font.BOLD, 12));
+
+        FontMetrics metrics = g.getFontMetrics();
+        g.drawString("Current Font: " + g.getFont(), 10, 30);
+        g.drawString("Ascent: " + metrics.getAscent(), 10, 45);
+        g.drawString("Descent: " + metrics.getDescent(), 10, 60);
+        g.drawString("Height: " + metrics.getHeight(), 10, 75);
+        g.drawString("Leading: " + metrics.getLeading(), 10, 90);
+
+        Font font = new Font("Serif", Font.ITALIC, 14);
+        metrics = g.getFontMetrics(font);
+        g.drawString("Current Font: " + font, 10, 120);
+        g.drawString("Ascent: " + metrics.getAscent(), 10, 135);
+        g.drawString("Descent: " + metrics.getDescent(), 10, 150);
+        g.drawString("Height: " + metrics.getHeight(), 10, 165);
+        g.drawString("Leading: " + metrics.getLeading(), 10, 180);
+    }
+}
