daebc20152eeaa9c0a581b6ba6b274c200683189
Mamithi <19lawrence93@gmail.com>
2018-07-12 18:45:52 +0300

    String Method valueOf

--- StringsAndRegularExpressions/src/com/mamithi/StringMiscellaneous2.java
+++ StringsAndRegularExpressions/src/com/mamithi/StringMiscellaneous2.java
@@ -4,7 +4,7 @@ public class StringMiscellaneous2 {
     public static void main(String[] args){
         String s1 = "hello";
         String s2 = "GOODBYE";
-        String s3 = "   spaces  ";
+        String s3 = "   spaces  and this ";
 
         System.out.printf("s1 = %s\ns2 = %s\ns3 = %s\n\n", s1, s2, s3);
 
