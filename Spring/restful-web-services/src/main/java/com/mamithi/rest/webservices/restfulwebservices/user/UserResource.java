2f972ebda3e8eaac5e13803bb2ec941cc6d36ef0
Mamithi <19lawrence93@gmail.com>
2018-08-12 23:28:28 +0300

    404 not found error handling

--- Spring/restful-web-services/src/main/java/com/mamithi/rest/webservices/restfulwebservices/user/UserResource.java
+++ Spring/restful-web-services/src/main/java/com/mamithi/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,12 @@
 package com.mamithi.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.xml.ws.Response;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,14 +23,26 @@ public class UserResource {
     // Retrieve one user
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if (user == null)
+            throw new UserNotFoundException("id-" + id);
+
+        return user;
+
     }
 
     // Create new user and return URI
     @PostMapping("/users")
-    public User createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@RequestBody User user){
         User savedUser =  service.save(user);
 
-        return savedUser;
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
