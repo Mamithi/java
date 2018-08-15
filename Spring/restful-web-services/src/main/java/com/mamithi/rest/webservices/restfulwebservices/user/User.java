4e5bbd216e0e1965f4c00ca2244daa9e9fe79a64
Mamithi <19lawrence93@gmail.com>
2018-08-15 15:20:47 +0300

    Using swagger for api documentation

--- Spring/restful-web-services/src/main/java/com/mamithi/rest/webservices/restfulwebservices/user/User.java
+++ Spring/restful-web-services/src/main/java/com/mamithi/rest/webservices/restfulwebservices/user/User.java
@@ -1,17 +1,24 @@
 package com.mamithi.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user")
 public class User {
 
     private Integer id;
 
+
     @Size(min = 2, message = "Name should have atleast 2 characters")
+    @ApiModelProperty(notes="Name should have atleast 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes="Birthdate should be in the past")
     private Date birthDate;
 
     protected User() {
