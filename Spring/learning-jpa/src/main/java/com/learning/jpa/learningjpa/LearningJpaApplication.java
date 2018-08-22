7ecbce6594ef55167845e3adddf54582db4c7989
Mamithi <19lawrence93@gmail.com>
2018-08-22 18:33:45 +0300

    Learning JPA

---
+++ Spring/learning-jpa/src/main/java/com/learning/jpa/learningjpa/LearningJpaApplication.java
@@ -0,0 +1,12 @@
+package com.learning.jpa.learningjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJpaApplication.class, args);
+	}
+}
---
+++ Spring/learning-jpa/src/main/java/com/learning/jpa/learningjpa/service/UserDAOService.java
@@ -0,0 +1,24 @@
+package com.learning.jpa.learningjpa.service;
+
+import com.learning.jpa.learningjpa.entity.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+
+@Repository
+@Transactional
+public class UserDAOService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public long insert(User user) {
+        entityManager.persist(user);
+
+        return user.getId();
+    }
+
+}
