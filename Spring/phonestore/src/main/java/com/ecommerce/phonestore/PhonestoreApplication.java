ca11d0fca700f076792826a7892dd2779020a7db
Mamithi <19lawrence93@gmail.com>
2018-08-23 14:00:20 +0300

    Creating navbar for ecommerce

---
+++ Spring/phonestore/src/main/java/com/ecommerce/phonestore/PhonestoreApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.phonestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonestoreApplication.class, args);
+	}
+}
